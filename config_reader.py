286ad2967a8018f31045de8b1616dd3c173c6780
Robert J Wynn <wynn.134@osu.edu>
2020-04-23 14:53:36 -0400

    Read config for different build types

--- config_reader.py
+++ config_reader.py
@@ -8,6 +8,10 @@ class JsonConfigReader:
         self._config_file = file
         self._data = None
         self._last_changed = 0
+        version = os.getenv('PAMUS_BUILD')
+        if version != 'release':
+            version = 'dev'
+        self._build = version
         self.update()
 
     def update(self):
@@ -19,15 +23,24 @@ class JsonConfigReader:
             self._last_changed = stamp
             with open(self._config_file, 'r') as config:
                 self._data = json.load(config)
-            self.__dict__.update(self._data)
+            data_clone = dict(self._data)
+            for key, value in self._data.items():
+                if isinstance(value, dict) and 'release' in value \
+                        and 'dev' in value:
+                    data_clone[key] = self._data[key][self._build]
+            self.__dict__.update(data_clone)
         finally:
             JsonConfigReader._lock.release()
 
     def save(self):
         JsonConfigReader._lock.acquire()
         try:
-            for k in self._data.keys():
-                self._data[k] = self.__dict__[k]
+            for key, value in self._data.items():
+                if isinstance(value, dict) and 'release' in value \
+                        and 'dev' in value:
+                    self._data[key][self._build] = self.__dict__[key]
+                else:
+                    self._data[key] = self.__dict__[key]
             with open(self._config_file, 'w') as config:
                 json.dump(self._data, config, sort_keys=True,
                           indent=4, separators=(',', ': '))
