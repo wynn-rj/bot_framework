566c8fcdf605fd5ca5dbba59411575bb28381425
Robert J Wynn <wynn.134@osu.edu>
2020-04-27 01:53:20 -0400

    Add update commands addon

---
+++ addons/update_commands.py
@@ -0,0 +1,52 @@
+import os
+import subprocess
+import discord
+from discord.ext import commands
+from utils import YAMLConfigReader, Logger
+
+class UpdateCommands(commands.Cog, command_attrs=dict(hidden=True)):
+    def __init__(self, bot):
+        self.bot = bot
+        self.reader = YAMLConfigReader('/config/administration.yml', defaults={
+            'admins': [],
+        })
+
+    def _allow_command(self, ctx):
+        self.reader.update()
+        return not str(ctx.author) in self.reader.data.admins
+
+    @commands.group(pass_context=True)
+    async def admin(self, ctx):
+        pass
+
+    @admin.command()
+    async def help(self, ctx):
+        if self._allow_command(ctx):
+            return
+        await ctx.send('Commands:\n - ' + '\n - '.join(
+            [str(cmd) for cmd in self.get_commands()[0].commands]))
+
+    @admin.command(name='reload_extensions')
+    async def reload_extensions(self, ctx):
+        if self._allow_command(ctx):
+            return
+        if os.path.exists('/app/extensions/.git'):
+            res = subprocess.run(['git', 'pull'], cwd='/app/extensions')
+            if res.returncode != 0:
+                Logger.log('Failed to reload update extensions')
+        config = YAMLConfigReader().data
+        result_msg = 'Extensions have been reloaderd:'
+        for extension in config.extensions:
+            result = "Success"
+            try:
+                self.bot.reload_extension(extension)
+            except commands.ExtensionNotLoaded:
+                self.bot.load_extensions(extension)
+            except commands.ExtensionError as exception:
+                result = str(exception)
+            result_msg += f'\n - {extension}: {result}'
+        await ctx.send(result_msg)
+        await Logger.log(result_msg)
+
+def setup(bot):
+    bot.add_cog(UpdateCommands(bot))
