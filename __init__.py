1035fe29a7268322674378fd11cd660a6052307f
Robert J Wynn <wynn.134@osu.edu>
2020-04-26 23:07:50 -0400

    Cleanup files

--- __init__.py
+++
@@ -1,8 +0,0 @@
-from .version import __version__
-from .config_reader import JsonConfigReader
-from .logger import Logger
-
-try:
-    from version import __version__ as bot_version
-except:
-    bot_version = "Not set"
